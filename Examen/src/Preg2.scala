fa39ec810b981913b8941c9279e06dddf847e872
OscarBurneo <oabr08@outlook.com>
2025-11-14 16:45:49 -0500

    Primera Parte act final

--- Examen/src/Preg2.scala
+++ Examen/src/Preg2.scala
@@ -1,7 +1,13 @@
+@main
 
-object Preg2 {
-  def main(): Unit = {
-    def funcion(precios: List[Double],)=
+  def main2(): Unit = {
+    val precios = List[Double](10, 20, 30, 40)
+    def funcion(precios: List[Double],regla:Double=> Double):List[Double]={
+      precios.map(regla)
+    }
+    def iva(cant:Double):Double=cant*(15/100)
+
+    println(funcion(precios,iva))
   }
-}
+
 
--- Examen/src/Preg3.scala
+++ Examen/src/Preg3.scala
@@ -1,10 +1,9 @@
-
-object Preg3 {
-  def main(): Unit = {
+@main
+  def main3(): Unit = {
     val generadorIncrementadorAnonimo: Int => (Int => Int) =
       (incremento: Int) =>(x: Int) => x + incremento
 
     def generadorIncrementador(Incremento:Int ,x:Int):Int=x+Incremento
   }
-}
+
 
